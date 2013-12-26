203e77ff36c46d305a9a65d1c27a3f9d059b96a2
brandly <brandly13@gmail.com>
2013-12-26 14:57:05 -0500

    update help page and test it

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
+
     it "should have the content 'Sample App'" do
       # `visit` is a Capybara function
       visit '/static_pages/home'
       # `page` is a Capybara variable
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
   end
 end
