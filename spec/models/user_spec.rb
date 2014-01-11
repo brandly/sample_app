65a5496ec864fa19a322ffe6d0849a8edc75f0fd
brandly <brandly13@gmail.com>
2014-01-11 18:47:05 -0500

    enforce uniqueness of emails at the ruby level

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,4 +46,23 @@ describe User do
       end
     end
   end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when email address is already taken, case insensitive" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
