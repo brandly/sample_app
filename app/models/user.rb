3ae94efe5757c0ae570923d9e80a3b4aa701a80f
brandly <brandly13@gmail.com>
2014-01-11 19:38:34 -0500

    demonstrate has_secure_password through tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  # Presence validations for the password and its confirmation are
+  # automatically added by has_secure_password.
+  validates :password, length: { minimum: 6 }
 
-  before_save { self.email = email.downcase }
   has_secure_password
 end
