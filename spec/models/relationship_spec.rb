71e9dbd887377ad584e2bfbc1e7a6ed7a82b2804
brandly <brandly13@gmail.com>
2014-01-21 22:37:58 -0500

    validate relationships

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -16,4 +16,14 @@ describe Relationship do
     its(:follower) { should eq follower }
     its(:followed) { should eq followed }
   end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
