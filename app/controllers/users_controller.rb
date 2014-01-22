a0adc7c7bf5df17ec094619189aae152892f43fc
brandly <brandly13@gmail.com>
2014-01-22 00:13:20 -0500

    un/follow buttons are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   before_action :not_signed_in,  only: [:new, :create]
